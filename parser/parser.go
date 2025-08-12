f52b1d4ffb8b77d0e1ba34dc6edfc2188d26f215
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-08-12 19:43:14 +0200

    Added helper for checking if input included in provided options

--- parser/parser.go
+++ parser/parser.go
@@ -12,9 +12,10 @@ type WorkFlow struct {
 	Tree map[string]any
 	Data *map[string]any
 
-	CurrentScreen  string
-	NextScreen     string
-	PreviousScreen string
+	CurrentScreen   string
+	NextScreen      string
+	PreviousScreen  string
+	CurrentLanguage string
 }
 
 func NewWorkflow(tree map[string]any) *WorkFlow {
@@ -34,6 +35,53 @@ func (w *WorkFlow) GetNode(screen string) map[string]any {
 	}
 
 	return nil
+}
+
+func (w *WorkFlow) InputIncluded(input string, options []map[string]any) bool {
+	found := false
+
+	for _, option := range options {
+		if option["position"] != nil {
+			if val, ok := option["position"].(int); ok && fmt.Sprint(val) == input {
+				found = true
+				break
+			}
+		}
+	}
+
+	return found
+}
+
+func (w *WorkFlow) NodeOptions(input string) []string {
+	options := []string{}
+
+	node := w.GetNode(input)
+	if node != nil && node["options"] != nil {
+		opts, ok := node["options"].([]any)
+		if ok {
+			for _, row := range opts {
+				optVal, ok := row.(map[string]any)
+				if ok {
+					position := fmt.Sprintf("%v", optVal["position"])
+
+					val, ok := optVal["label"].(map[string]any)
+					if ok {
+						if val["all"] != nil {
+							entry := fmt.Sprintf("%s. %s", position, val["all"])
+
+							options = append(options, entry)
+						} else if w.CurrentLanguage != "" && val[w.CurrentLanguage] != nil {
+							entry := fmt.Sprintf("%s. %s", position, val[w.CurrentLanguage])
+
+							options = append(options, entry)
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return options
 }
 
 func (w *WorkFlow) NextNode(input string) map[string]any {
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -43,6 +43,81 @@ func TestGetNode(t *testing.T) {
 	}
 }
 
+func TestInputIncluded(t *testing.T) {
+	options := []map[string]any{
+		{
+			"position": 1,
+			"label": map[string]any{
+				"all": "English",
+			},
+		},
+		{
+			"position": 2,
+			"label": map[string]any{
+				"all": "Chichewa",
+			},
+		},
+	}
+
+	wf := parser.NewWorkflow(data)
+
+	result := wf.InputIncluded("3", options)
+
+	if result {
+		t.Fatalf("Test failed. Expected: false; Actual: %v", result)
+	}
+
+	result = wf.InputIncluded("1", options)
+
+	if !result {
+		t.Fatalf("Test failed. Expected: true; Actual: %v", result)
+	}
+}
+
+func TestNodeOptions(t *testing.T) {
+	wf := parser.NewWorkflow(data)
+
+	result := wf.NodeOptions("enterLanguage")
+
+	if len(result) != 2 {
+		t.Fatalf("Test failed. Expected: 2; Actual: %v", len(result))
+	}
+
+	for i, entry := range []string{"1. English", "2. Chichewa"} {
+		if result[i] != entry {
+			t.Fatalf("Test failed. Expected: %s; Actual: %s", entry, result[i])
+		}
+	}
+
+	wf.CurrentLanguage = "ny"
+
+	result = wf.NodeOptions("enterMaritalStatus")
+
+	if len(result) != 4 {
+		t.Fatalf("Test failed. Expected: 4; Actual: %v", len(result))
+	}
+
+	for i, entry := range []string{"1. Inde", "2. Ayi", "3. Woferedwa", "4. Osudzulidwa"} {
+		if result[i] != entry {
+			t.Fatalf("Test failed. Expected: %s; Actual: %s", entry, result[i])
+		}
+	}
+
+	wf.CurrentLanguage = "en"
+
+	result = wf.NodeOptions("enterMaritalStatus")
+
+	if len(result) != 4 {
+		t.Fatalf("Test failed. Expected: 4; Actual: %v", len(result))
+	}
+
+	for i, entry := range []string{"1. Married", "2. Single", "3. Widowed", "4. Divorced"} {
+		if result[i] != entry {
+			t.Fatalf("Test failed. Expected: %s; Actual: %s", entry, result[i])
+		}
+	}
+}
+
 func TestNextNode(t *testing.T) {
 	wf := parser.NewWorkflow(data)
 
@@ -67,6 +142,20 @@ func TestNextNode(t *testing.T) {
 	}
 
 	result = wf.NextNode("3")
+
+	for _, key := range []string{"type", "text", "options", "inputIdentifier", "nextScreen"} {
+		if result[key] == nil {
+			t.Fatalf("Test failed on key %s", key)
+		}
+	}
+
+	if wf.CurrentScreen != "enterLanguage" {
+		t.Fatalf("Test failed. Expected: 'enterLanguage'; Actual: '%v'", wf.CurrentScreen)
+	}
+
+	if wf.PreviousScreen != "initialScreen" {
+		t.Fatalf("Test failed. Expected: 'initialScreen'; Actual: '%v'", wf.PreviousScreen)
+	}
 
 	fmt.Printf("%#v\n", result)
 }
