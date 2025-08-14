7949a909a3459a048ae5d98a0ff83f11125f4180
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-08-14 16:17:18 +0200

    Added Sqlite3 database support

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	go.mau.fi/whatsmeow v0.0.0-20250807072145-72ce90b82194
 	google.golang.org/protobuf v1.36.7
 	gopkg.in/yaml.v3 v3.0.1
+	modernc.org/sqlite v1.38.2
 )
 
 require (
@@ -17,6 +18,7 @@ require (
 	fyne.io/systray v1.11.0 // indirect
 	github.com/BurntSushi/toml v1.4.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/fredbi/uri v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/fyne-io/gl-js v0.2.0 // indirect
@@ -35,10 +37,12 @@ require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/nicksnyder/go-i18n/v2 v2.5.1 // indirect
 	github.com/petermattis/goid v0.0.0-20250508124226-395b08cebbdb // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/rs/zerolog v1.34.0 // indirect
 	github.com/rymdport/portal v0.4.1 // indirect
 	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
@@ -53,4 +57,7 @@ require (
 	golang.org/x/net v0.42.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
+	modernc.org/libc v1.66.3 // indirect
+	modernc.org/mathutil v1.7.1 // indirect
+	modernc.org/memory v1.11.0 // indirect
 )
---
+++ server/database/database.go
@@ -0,0 +1,93 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "modernc.org/sqlite"
+)
+
+type Database struct {
+	DbName string
+	DB     *sql.DB
+}
+
+func NewDatabase(dbname string) *Database {
+	db, err := sql.Open("sqlite", fmt.Sprintf("%s.db", dbname))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	instance := &Database{
+		DbName: dbname,
+		DB:     db,
+	}
+
+	err = instance.initDb()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return instance
+}
+
+func (d *Database) Close() {
+	d.DB.Close()
+}
+
+func (d *Database) initDb() error {
+	sqlStmt := `
+		CREATE TABLE IF NOT EXISTS member (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			firstName TEXT,
+			lastName TEXT,
+			otherName TEXT,
+			gender TEXT,
+			title TEXT
+			maritalStatus TEXT,
+			dateOfBirth TEXT,
+			nationalId TEXT,
+			utilityBillType TEXT,
+			utilityBillNumber TEXT
+		);
+		CREATE TABLE IF NOT EXISTS memberContact (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			memberId INTEGER NOT NULL,
+			postalAddress TEXT,
+			residentialAddress TEXT,
+			phoneNumber TEXT,
+			homeVillage TEXT,
+			homeTA TEXT,
+			homeDistrict TEXT
+		);
+		CREATE TABLE IF NOT EXISTS memberNominee (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			memberId INTEGER NOT NULL,
+			nextOfKinName TEXT,
+			nextOfKinPhone TEXT,
+			nextOfKinAddress TEXT
+		);
+		CREATE TABLE IF NOT EXISTS memberOccupation (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			memberId INTEGER NOT NULL,
+			employerName TEXT,
+			netPay REAL,
+			jobTitle TEXT,
+			employerAddress TEXT,
+			highestQualification TEXT
+		);
+		CREATE TABLE IF NOT EXISTS memberBeneficiary (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			memberId INTEGER NOT NULL,
+			name TEXT,
+			percentage REAL,
+			contact TEXT
+		);
+	`
+	_, err := d.DB.Exec(sqlStmt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
