196bf1599679f66f79339f79f9d962822f0435b4
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-08-14 18:45:25 +0200

    Cleaned up database unit tests

--- server/database/database_test.go
+++ server/database/database_test.go
@@ -1,12 +1,9 @@
 package database_test
 
 import (
-	"database/sql"
 	"os"
 	"sacco/server/database"
-	"sacco/utils"
 	"slices"
-	"strings"
 	"testing"
 )
 
@@ -22,35 +19,6 @@ func TestDatabase(t *testing.T) {
 			os.Remove("test.db")
 		}
 	}()
-
-	row := db.DB.QueryRow(`SELECT sql FROM sqlite_schema WHERE name="member"`)
-
-	target := utils.CleanString(`
-CREATE TABLE member (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	firstName TEXT,
-	lastName TEXT,
-	otherName TEXT,
-	gender TEXT,
-	title TEXT
-	maritalStatus TEXT,
-	dateOfBirth TEXT,
-	nationalId TEXT,
-	utilityBillType TEXT,
-	utilityBillNumber TEXT
-)`)
-
-	var result string
-
-	if err := row.Scan(&result); err == sql.ErrNoRows {
-		t.Fatal("Test failed")
-	}
-
-	result = utils.CleanString(result)
-
-	if strings.Compare(result, target) != 0 {
-		t.Fatal("Test failed")
-	}
 
 	rows, err := db.DB.QueryContext(t.Context(), `SELECT name FROM sqlite_schema WHERE type="table"`)
 	if err != nil {
