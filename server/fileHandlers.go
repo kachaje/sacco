a8b7abdb2a9c704e9dd99a9c50aabad5c7995fe7
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-08-15 16:37:57 +0200

    More updates for server logic for saving data

---
+++ server/fileHandlers.go
@@ -0,0 +1,286 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"sacco/server/menus"
+	"strconv"
+)
+
+func cacheFile(filename string, data any) {
+	payload, err := json.MarshalIndent(data, "", "  ")
+	if err == nil {
+		err = os.WriteFile(filename, payload, 0644)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	} else {
+		log.Println(err)
+	}
+}
+
+func saveData(data any, model, phoneNumber, sessionId *string) {
+	sessionFolder := filepath.Join(cacheFolder, *phoneNumber, *sessionId)
+
+	_, err := os.Stat(sessionFolder)
+	if os.IsNotExist(err) {
+		os.MkdirAll(sessionFolder, 0755)
+	}
+
+	switch *model {
+	case "preferredLanguage":
+		val, ok := data.(map[string]any)
+		if ok {
+			if val["language"] != nil && phoneNumber != nil {
+				language, ok := val["language"].(string)
+				if ok {
+					savePreference(*phoneNumber, "language", language)
+				}
+			}
+		}
+
+	case "memberDetails":
+		memberData, ok := data.(map[string]any)
+		if ok {
+			var id int64
+			var err error
+
+			if menus.Sessions[*sessionId].ContactsAdded ||
+				menus.Sessions[*sessionId].BeneficiariesAdded ||
+				menus.Sessions[*sessionId].NomineeAdded ||
+				menus.Sessions[*sessionId].OccupationAdded {
+				var contactsData, nomineeData, occupationData map[string]any
+				var beneficiariesData []map[string]any
+
+				contactsFile := filepath.Join(sessionFolder, "contactDetails.json")
+
+				_, err = os.Stat(contactsFile)
+				if !os.IsNotExist(err) {
+					content, err := os.ReadFile(contactsFile)
+					if err != nil {
+						log.Println(err)
+					} else {
+						err = json.Unmarshal(content, &contactsData)
+						if err != nil {
+							log.Println(err)
+						}
+					}
+				}
+
+				nomineeFile := filepath.Join(sessionFolder, "nomineeDetails.json")
+				if !os.IsNotExist(err) {
+					content, err := os.ReadFile(nomineeFile)
+					if err != nil {
+						log.Println(err)
+					} else {
+						err = json.Unmarshal(content, &nomineeData)
+						if err != nil {
+							log.Println(err)
+						}
+					}
+				}
+
+				occupationFile := filepath.Join(sessionFolder, "occupationalDetails.json")
+				if !os.IsNotExist(err) {
+					content, err := os.ReadFile(occupationFile)
+					if err != nil {
+						log.Println(err)
+					} else {
+						err = json.Unmarshal(content, &occupationData)
+						if err != nil {
+							log.Println(err)
+						}
+					}
+				}
+
+				beneficiariesFile := filepath.Join(sessionFolder, "beneficiaries.json")
+				if !os.IsNotExist(err) {
+					content, err := os.ReadFile(beneficiariesFile)
+					if err != nil {
+						log.Println(err)
+					} else {
+						err = json.Unmarshal(content, &beneficiariesData)
+						if err != nil {
+							log.Println(err)
+						}
+					}
+				}
+
+				mid, err := db.AddMember(memberData, contactsData, nomineeData, occupationData, beneficiariesData, nil)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+
+				id = *mid
+			} else {
+				id, err = db.Member.AddMember(memberData)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+			}
+
+			menus.Sessions[*sessionId].MemberId = &id
+
+			filename := filepath.Join(sessionFolder, "memberDetails.json")
+
+			memberData["id"] = id
+
+			cacheFile(filename, memberData)
+		}
+
+	case "contactDetails":
+		val, ok := data.(map[string]any)
+		if ok {
+			if menus.Sessions[*sessionId].MemberId != nil {
+				val["memberId"] = *menus.Sessions[*sessionId].MemberId
+
+				_, err := db.AddMember(nil, val, nil, nil, nil, menus.Sessions[*sessionId].MemberId)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+			}
+
+			filename := filepath.Join(sessionFolder, "contactDetails.json")
+
+			cacheFile(filename, val)
+
+			menus.Sessions[*sessionId].ContactsAdded = true
+		}
+
+	case "nomineeDetails":
+		val, ok := data.(map[string]any)
+		if ok {
+			if menus.Sessions[*sessionId].MemberId != nil {
+				val["memberId"] = *menus.Sessions[*sessionId].MemberId
+
+				_, err := db.AddMember(nil, nil, val, nil, nil, menus.Sessions[*sessionId].MemberId)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+			}
+
+			filename := filepath.Join(sessionFolder, "nomineeDetails.json")
+
+			cacheFile(filename, val)
+
+			menus.Sessions[*sessionId].NomineeAdded = true
+		}
+
+	case "occupationalDetails":
+		val, ok := data.(map[string]any)
+		if ok {
+			if menus.Sessions[*sessionId].MemberId != nil {
+				val["memberId"] = *menus.Sessions[*sessionId].MemberId
+
+				_, err := db.AddMember(nil, nil, nil, val, nil, menus.Sessions[*sessionId].MemberId)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+			}
+
+			filename := filepath.Join(sessionFolder, "occupationalDetails.json")
+
+			cacheFile(filename, val)
+
+			menus.Sessions[*sessionId].OccupationAdded = true
+		}
+
+	case "beneficiaries":
+		rawData, ok := data.(map[string]any)
+		if ok {
+			records := []map[string]any{}
+
+			for i := range 4 {
+				var name, contact string
+				var percentage float64
+
+				index := i + 1
+
+				nameLabel := fmt.Sprintf("beneficiary%vName", index)
+				percentLabel := fmt.Sprintf("beneficiary%vPercent", index)
+				contactLabel := fmt.Sprintf("beneficiary%vContact", index)
+
+				if rawData[nameLabel] == nil {
+					break
+				}
+
+				name = fmt.Sprintf("%v", rawData[nameLabel])
+				contact = fmt.Sprintf("%v", rawData[contactLabel])
+
+				v, err := strconv.ParseFloat(fmt.Sprintf("%v", rawData[percentLabel]), 64)
+				if err == nil {
+					percentage = v
+				} else {
+					log.Println(err)
+				}
+
+				row := map[string]any{
+					"name":       name,
+					"percentage": percentage,
+					"contact":    contact,
+				}
+
+				if menus.Sessions[*sessionId].MemberId != nil {
+					row["memberId"] = *menus.Sessions[*sessionId].MemberId
+				}
+
+				records = append(records, row)
+			}
+
+			_, err := db.AddMember(nil, nil, nil, nil, records, menus.Sessions[*sessionId].MemberId)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			filename := filepath.Join(sessionFolder, "beneficiaries.json")
+
+			cacheFile(filename, records)
+
+			menus.Sessions[*sessionId].BeneficiariesAdded = true
+		}
+
+	default:
+		fmt.Println("##########", *phoneNumber, *sessionId, data)
+	}
+}
+
+func savePreference(phoneNumber, key, value string) error {
+	settingsFile := filepath.Join(preferencesFolder, phoneNumber)
+
+	data := map[string]any{}
+
+	_, err := os.Stat(settingsFile)
+	if !os.IsNotExist(err) {
+		content, err := os.ReadFile(settingsFile)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+
+		err = json.Unmarshal(content, &data)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+	}
+
+	data[key] = value
+
+	payload, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return os.WriteFile(settingsFile, payload, 0644)
+}
