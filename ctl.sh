b20f375194968624d1543221d5c36e8c375f2720
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-08-18 18:22:23 +0200

    Added logic for building cli app

--- ctl.sh
+++ ctl.sh
@@ -4,6 +4,10 @@ if [[ "$1" == "-b" ]]; then
 
     fyne package -os android -app-id com.example.kaso -icon Icon.png
 
+elif [[ "$1" == "-bc" ]]; then
+
+    go build -o cli cmd/wscli/*.go
+
 elif [[ "$1" == "-c" ]]; then
 
     rm -rf settings/ **/**/settings/ **/**/data/ **/**/*.db
