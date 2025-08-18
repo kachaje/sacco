f00cd8ebdef4b08ab73d29f78cf5e4f3fd40d3a9
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-08-18 21:30:03 +0200

    Added unit test for SaveData

--- server/fileHandlers_test.go
+++ server/fileHandlers_test.go
@@ -1,7 +1,35 @@
 package server_test
 
-import "testing"
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+)
 
 func TestSaveData(t *testing.T) {
+	phoneNumber := "0999888777"
+	sourceFolder := filepath.Join(".", "database", "models", "fixtures", "cache", phoneNumber)
+	cacheFolder := filepath.Join(".", "tmp", "cache", phoneNumber)
 
+	os.MkdirAll(cacheFolder, 0755)
+
+	for _, file := range []string{"contactDetails.json"} {
+		src, err := os.Open(filepath.Join(sourceFolder, file))
+		if err != nil {
+			continue
+		}
+		defer src.Close()
+
+		dst, err := os.Create(filepath.Join(cacheFolder, file))
+		if err != nil {
+			continue
+		}
+		defer dst.Close()
+
+		_, err = io.Copy(dst, src)
+		if err != nil {
+			continue
+		}
+	}
 }
