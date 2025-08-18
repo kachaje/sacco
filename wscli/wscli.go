4c09ffea65ec40c3730cbf0a328b1fb6b879b21a
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-08-18 16:38:28 +0200

    Added standalone ws cli client

---
+++ wscli/wscli.go
@@ -0,0 +1,84 @@
+package wscli
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+	"slices"
+
+	"github.com/gorilla/websocket"
+)
+
+func clearScreen() {
+	var cmd *exec.Cmd
+
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		cmd = exec.Command("clear")
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func Main() {
+	var port int64 = 8080
+	var phoneNumber string = "cli"
+	var silentMode bool
+
+	flag.Int64Var(&port, "p", port, "server port")
+	flag.StringVar(&phoneNumber, "n", phoneNumber, "phone number")
+	flag.BoolVar(&silentMode, "s", silentMode, "silent mode")
+
+	flag.Parse()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	client, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://localhost:%d/ws?phoneNumber=%s", port, phoneNumber), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			clearScreen()
+
+			_, message, err := client.ReadMessage()
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+
+			if slices.Contains([]string{
+				"Thank you for using our service",
+				"Zikomo potidalila",
+			}, string(message)) {
+				return
+			} else if !silentMode {
+				fmt.Println(string(message))
+			}
+
+			scanner.Scan()
+
+			input := scanner.Text()
+
+			err = client.WriteMessage(websocket.TextMessage, []byte(input))
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+		}
+	}
+}
