470b04e9498f80fe667acacc9717b2b5c8f1ed0a
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-08-19 22:29:37 +0200

    More updates for helpers for loading template data

--- server/menus/helpers.go
+++ server/menus/helpers.go
@@ -1,9 +1,90 @@
 package menus
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func LoadTemplateData(data map[string]any, template map[string]any) map[string]any {
 	result := map[string]any{}
 
-	
+	for key, value := range template {
+		result[key] = map[string]any{}
+
+		var level string
+
+		val, ok := value.(map[string]any)
+		if ok {
+			if val["level"] != nil {
+				level = fmt.Sprintf("%v", val["level"])
+			}
+
+			fieldData := map[string]any{}
+
+			v, ok := val["data"].(map[string]any)
+			if ok {
+				fieldData = v
+			}
+
+			switch level {
+			case "beneficiaries":
+				v, ok := data[level].([]any)
+				if ok {
+					j := 1
+					for i := range v {
+						vd, ok := v[i].(map[string]any)
+						if ok {
+							for field, kids := range fieldData {
+								vf, ok := kids.(map[string]any)
+								if ok {
+									keyLabel := fmt.Sprintf("%s%d", field, i+1)
+
+									result[key].(map[string]any)[keyLabel] = map[string]any{
+										"order": j,
+										"label": fmt.Sprintf("%v", vf["label"]),
+									}
+
+									if vd[field] != nil {
+										result[key].(map[string]any)[keyLabel].(map[string]any)["value"] = vd[field]
+									}
+
+									j++
+								}
+							}
+						}
+					}
+				}
+			default:
+				for field, kids := range fieldData {
+					vf, ok := kids.(map[string]any)
+					if ok {
+						order, err := strconv.ParseInt(fmt.Sprintf("%v", vf["order"]), 10, 64)
+						if err == nil {
+							result[key].(map[string]any)[field] = map[string]any{
+								"order": order,
+								"label": fmt.Sprintf("%v", vf["label"]),
+							}
+
+							if level == "root" {
+								if data[field] != nil && fmt.Sprintf("%v", data[field]) != "" {
+									result[key].(map[string]any)[field].(map[string]any)["value"] = data[field]
+								}
+							} else {
+								if data[level] != nil {
+									v, ok := data[level].(map[string]any)
+									if ok {
+										if v[field] != nil {
+											result[key].(map[string]any)[field].(map[string]any)["value"] = v[field]
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 
 	return result
 }
--- server/menus/helpers_test.go
+++ server/menus/helpers_test.go
@@ -1,7 +1,41 @@
 package menus_test
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sacco/server/menus"
+	"testing"
+)
 
-func TestTabulateData(t *testing.T) {
-	
+func TestLoadTemplateData(t *testing.T) {
+	data := map[string]any{}
+	templateData := map[string]any{}
+
+	content, err := os.ReadFile(filepath.Join("..", "database", "models", "fixtures", "member.json"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = json.Unmarshal(content, &data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	content, err = os.ReadFile(filepath.Join(".", "templates", "member.template.json"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = json.Unmarshal(content, &templateData)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	result := menus.LoadTemplateData(data, templateData)
+
+	payload, _ := json.MarshalIndent(result, "", "  ")
+
+	fmt.Println(string(payload))
 }
