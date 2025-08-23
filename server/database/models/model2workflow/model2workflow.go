628bb4f060a6662c131eaa3ba03eda7cdd39cebc
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-08-23 20:58:33 +0200

    More updates for model2workflow

--- server/database/models/model2workflow/model2workflow.go
+++ server/database/models/model2workflow/model2workflow.go
@@ -1,5 +1,26 @@
 package model2workflow
 
-func Main() {
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"sacco/utils"
+)
 
+func Main(model, sourceFile, destinationFile string) error {
+	content, err := os.ReadFile(sourceFile)
+	if err != nil {
+		return err
+	}
+
+	data, err := utils.LoadYaml(string(content))
+	if err != nil {
+		return err
+	}
+
+	payload, _ := json.MarshalIndent(data[model], "", "  ")
+
+	fmt.Println(string(payload))
+
+	return nil
 }
--- server/database/models/model2workflow/model2workflow_test.go
+++ server/database/models/model2workflow/model2workflow_test.go
@@ -1,3 +1,27 @@
 package model2workflow_test
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sacco/server/database/models/model2workflow"
+	"testing"
+)
 
+func TestModel2Workflow(t *testing.T) {
+	workingFolder := filepath.Join(".", "tmpM2W")
+
+	model := "member"
+	srcFile := filepath.Join(".", "fixtures", "models.yml")
+	dstFile := filepath.Join(workingFolder, fmt.Sprintf("%s.yml", model))
+
+	err := os.MkdirAll(workingFolder, 0755)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = model2workflow.Main(model, srcFile, dstFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
