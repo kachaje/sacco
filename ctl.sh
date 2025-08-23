fc4ee5d4980faaed954de8f41192b41033bd9a91
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-08-23 15:43:13 +0200

    Added script for running unit test coverage

--- ctl.sh
+++ ctl.sh
@@ -12,4 +12,10 @@ elif [[ "$1" == "-c" ]]; then
 
     rm -rf settings/ **/**/settings/ **/**/data/ **/**/*.db **/tmp*/
 
+elif [[ "$1" == "-cov" ]]; then
+
+    go test -coverprofile=coverage.out ./...
+
+    go tool cover -html=coverage.out
+
 fi
