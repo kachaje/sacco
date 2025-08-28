e76d64b2d295ea22dc1eb66ccfcc53d19e4f1f24
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-08-28 09:07:15 +0200

    Refactored out listUsers function

--- server/menus/menu.go
+++ server/menus/menu.go
@@ -116,7 +116,7 @@ func NewMenus(devMode, demoMode *bool) *Menus {
 		return menufuncs.SignIn(m.LoadMenu, DB, data)
 	}
 	m.FunctionsMap["listUsers"] = func(data map[string]any) string {
-		return m.listUsers(data)
+		return menufuncs.ListUsers(m.LoadMenu, DB, data)
 	}
 	m.FunctionsMap["blockUser"] = func(data map[string]any) string {
 		return m.blockUser(data)
@@ -707,40 +707,6 @@ func (m *Menus) memberLoansSummary(data map[string]any) string {
 	var response string = "Member Loans Summary\n\n00. Main Menu"
 
 	_ = data
-
-	return response
-}
-
-func (m *Menus) listUsers(_ map[string]any) string {
-	var response, content string
-
-	title := "Users List\n----------"
-
-	result, err := DB.SQLQuery("SELECT id, username, role, created_at, updated_at FROM user WHERE active = 1")
-	if err != nil {
-		content = fmt.Sprintf("%s\n", err.Error())
-	} else {
-		rows := []string{
-			fmt.Sprintf("%2s | %-10s | %-8s | %-20s | %-20s", "id", "username", "role", "created_at", "updated_at"),
-			strings.Repeat("-", 70),
-		}
-
-		for _, row := range result {
-			id := fmt.Sprintf("%v", row["id"])
-			username := fmt.Sprintf("%v", row["username"])
-			role := fmt.Sprintf("%v", row["role"])
-			createdAt := fmt.Sprintf("%v", row["created_at"])
-			updatedAt := fmt.Sprintf("%v", row["updated_at"])
-
-			entry := fmt.Sprintf("%2s | %-10s | %-8s | %-20s | %-20s", id, username, role, createdAt, updatedAt)
-
-			rows = append(rows, entry)
-		}
-
-		content = strings.Join(rows, "\n")
-	}
-
-	response = fmt.Sprintf("%s\n\n%s\n\n00. Main Menu\n", title, content)
 
 	return response
 }
