ae1e09377617747e2dd23b5cf6834d256466ebd2
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-08-28 09:39:34 +0200

    Refactored out businessSummary function

---
+++ server/menus/menuFuncs/businessSummary_fn.go
@@ -0,0 +1,22 @@
+package menufuncs
+
+import (
+	"sacco/server/database"
+	"sacco/server/parser"
+)
+
+func BusinessSummary(
+	loadMenu func(
+		menuName string, session *parser.Session,
+		phoneNumber, text, preferencesFolder, cacheFolder string,
+	) string,
+	db *database.Database,
+	data map[string]any,
+) string {
+	var result string = "Business Summary\n\n" +
+		"00. Main Menu\n"
+
+	_ = data
+
+	return result
+}
