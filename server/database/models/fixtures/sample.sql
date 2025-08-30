6f8dfcc9cc856955894444b6924af28a5230ab3e
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-08-30 18:39:47 +0200

    Added sample test fixture for loading sample test data

---
+++ server/database/models/fixtures/sample.sql
@@ -0,0 +1,66 @@
+INSERT INTO
+  member (
+    id,
+    firstName,
+    lastName,
+    otherName,
+    gender,
+    title,
+    maritalStatus,
+    dateOfBirth,
+    nationalId,
+    utilityBillType,
+    utilityBillNumber,
+    fileNumber,
+    oldFileNumber,
+    phoneNumber
+  )
+VALUES
+  (
+    1,
+    "Mary",
+    "Banda",
+    "",
+    "Female",
+    "Miss",
+    "Single",
+    "1999-09-01",
+    "DHFYR8475",
+    "ESCOM",
+    "29383746",
+    "",
+    "",
+    "09999999999"
+  );
+
+INSERT INTO
+  memberContact (
+    memberId,
+    postalAddress,
+    residentialAddress,
+    homeVillage,
+    homeTraditionalAuthority,
+    homeDistrict
+  )
+VALUES
+  (
+    1,
+    "P.O. Box 1000, Lilongwe",
+    "Area 2, Lilongwe",
+    "Songwe",
+    "Kyungu",
+    "Karonga"
+  );
+
+INSERT INTO
+  memberNominee (memberId, name, phoneNumber, address)
+VALUES
+  (1, "John Banda", "0888888888", "Same as member");
+
+INSERT INTO
+  memberBeneficiary (memberId, name, percentage, contact)
+VALUES
+  (1, "Benefator 1", 10, "P.O. Box 1"),
+  (1, "Benefator 2", 8, "P.O. Box 2"),
+  (1, "Benefator 3", 5, "P.O. Box 3"),
+  (1, "Benefator 4", 2, "P.O. Box 4");
