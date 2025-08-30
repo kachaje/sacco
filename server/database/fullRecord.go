232cadd805b701d6e5a2471c9af9cd9717a9d2f3
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-08-30 18:44:15 +0200

    More updates for FullRecord helper

--- server/database/fullRecord.go
+++ server/database/fullRecord.go
@@ -1,7 +1,20 @@
 package database
 
+import "fmt"
+
 func (d *Database) FullRecord(phoneNumber string) (map[string]any, error) {
-	data := map[string]any{}
+	var data = map[string]any{}
+
+	results, err := d.GenericModels["member"].FilterBy(fmt.Sprintf(`WHERE phoneNumber = "%s" AND active = 1`, phoneNumber))
+	if err != nil {
+		return nil, err
+	}
+
+	if len(results) > 0 {
+		data = results[0]
+	} else {
+		return nil, fmt.Errorf("no match found")
+	}
 
 	return data, nil
 }
--- server/database/fullRecord_test.go
+++ server/database/fullRecord_test.go
@@ -1,13 +1,35 @@
 package database_test
 
 import (
+	"encoding/json"
+	"fmt"
 	"sacco/server/database"
 	"testing"
+
+	_ "embed"
 )
+
+//go:embed models/fixtures/sample.sql
+var sampleScript string
 
 func TestFullRecord(t *testing.T) {
 	dbname := ":memory:"
 	db := database.NewDatabase(dbname)
 	defer db.Close()
 
+	_, err := db.DB.Exec(sampleScript)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	phoneNumber := "09999999999"
+
+	result, err := db.FullRecord(phoneNumber)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	payload, _ := json.MarshalIndent(result, "", "  ")
+
+	fmt.Println(string(payload))
 }
