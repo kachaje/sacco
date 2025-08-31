f98dd407b8a9d44d0f2126da35948581f87e4938
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-08-31 12:49:56 +0200

    Added separate createSession.go file

---
+++ server/menus/menuFuncs/createSession.go
@@ -0,0 +1,37 @@
+package menufuncs
+
+import (
+	filehandling "sacco/server/fileHandling"
+	"sacco/server/parser"
+)
+
+func CreateNewSession(phoneNumber, sessionId, preferencesFolder, preferredLanguage string, demoMode bool) *parser.Session {
+	mu.Lock()
+	session, exists := Sessions[phoneNumber]
+	if !exists {
+		session = parser.NewSession(DB.MemberByPhoneNumber, &phoneNumber, &sessionId)
+
+		for model, data := range WorkflowsData {
+			session.WorkflowsMapping[model] = parser.NewWorkflow(data, filehandling.SaveData, &preferredLanguage, &phoneNumber, &sessionId, &preferencesFolder, DB.GenericsSaveData, Sessions, nil)
+		}
+
+		if preferredLanguage != "" {
+			session.PreferredLanguage = preferredLanguage
+		}
+
+		if demoMode {
+			defaultUser := "admin"
+			defaultUserId := int64(1)
+			defaultRole := "admin"
+
+			session.SessionUser = &defaultUser
+			session.SessionUserId = &defaultUserId
+			session.SessionUserRole = &defaultRole
+		}
+
+		Sessions[phoneNumber] = session
+	}
+	mu.Unlock()
+
+	return session
+}
--- server/menus/menuFuncs/setPhoneNumber_fn.go
+++ server/menus/menuFuncs/setPhoneNumber_fn.go
@@ -3,40 +3,8 @@ package menufuncs
 import (
 	"fmt"
 	"regexp"
-	filehandling "sacco/server/fileHandling"
 	"sacco/server/parser"
 )
-
-func CreateNewSession(phoneNumber, sessionId, preferencesFolder, preferredLanguage string, demoMode bool) *parser.Session {
-	mu.Lock()
-	session, exists := Sessions[phoneNumber]
-	if !exists {
-		session = parser.NewSession(DB.MemberByPhoneNumber, &phoneNumber, &sessionId)
-
-		for model, data := range WorkflowsData {
-			session.WorkflowsMapping[model] = parser.NewWorkflow(data, filehandling.SaveData, &preferredLanguage, &phoneNumber, &sessionId, &preferencesFolder, DB.GenericsSaveData, Sessions, nil)
-		}
-
-		if preferredLanguage != "" {
-			session.PreferredLanguage = preferredLanguage
-		}
-
-		if demoMode {
-			defaultUser := "admin"
-			defaultUserId := int64(1)
-			defaultRole := "admin"
-
-			session.SessionUser = &defaultUser
-			session.SessionUserId = &defaultUserId
-			session.SessionUserRole = &defaultRole
-		}
-
-		Sessions[phoneNumber] = session
-	}
-	mu.Unlock()
-
-	return session
-}
 
 func SetPhoneNumber(
 	loadMenu func(
