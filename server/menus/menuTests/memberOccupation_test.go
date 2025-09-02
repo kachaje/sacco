6d203b08173619827158b3f166f8e1cd14a1ff35
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-09-02 11:41:48 +0200

    More updates for menu unit tests

--- server/menus/menuTests/memberOccupation_test.go
+++ server/menus/menuTests/memberOccupation_test.go
@@ -11,8 +11,6 @@ import (
 )
 
 func TestMemberOccupation(t *testing.T) {
-	t.Skip()
-
 	var data map[string]any
 
 	refData := map[string]any{
@@ -35,8 +33,14 @@ func TestMemberOccupation(t *testing.T) {
 	session.SessionUserRole = &role
 
 	session.GlobalIds = map[string]any{
-		"memberId":     1,
-		"memberLoanId": 1,
+		"memberId": map[string]any{
+			"key":   "member.id",
+			"value": "1",
+		},
+		"memberLoanId": map[string]any{
+			"key":   "member.memberLoan.0.id",
+			"value": "1",
+		},
 	}
 
 	menufuncs.Sessions[phoneNumber] = session
@@ -72,6 +76,7 @@ CON Welcome to Kaso SACCO
 5. Preferred Language 
 6. Administration
 7. Exit
+9. Set PhoneNumber
 	`
 
 	if utils.CleanString(result) != utils.CleanString(target) {
@@ -104,9 +109,8 @@ CON Loans
 
 	result = m.LoadMenu(session.CurrentMenu, session, phoneNumber, text, "")
 
-	target = `
-CON Employement
-2. Employement Verification
+	target = `CON Employement
+1. Employement Details
 3. Employement Summary
 
 99. Cancel
@@ -117,7 +121,7 @@ CON Employement
 		t.Fatal("Test failed")
 	}
 
-	text = "6"
+	text = "1"
 
 	result = m.LoadMenu(session.CurrentMenu, session, phoneNumber, text, "")
 
@@ -278,223 +282,7 @@ Summary
 
 	result = m.LoadMenu(session.CurrentMenu, session, phoneNumber, text, "")
 
-	target = `
-CON Choose Activity
-1. Member Details 
-2. Occupation Details (*)
-3. Contact Details
-4. Next of Kin Details 
-5. Beneficiaries 
-6. View Member Details
-
-00. Main Menu
-	`
-
-	if utils.CleanString(result) != utils.CleanString(target) {
-		t.Fatal("Test failed")
-	}
-
-	if data == nil {
-		t.Fatal("Test failed")
-	}
-
-	if !reflect.DeepEqual(data, refData) {
-		t.Fatal("Test failed")
-	}
-
-	text = "2"
-
-	result = m.LoadMenu(session.CurrentMenu, session, phoneNumber, text, "")
-
-	target = `
-Employer Name: (SOBO)
-
-01. Keep
-99. Cancel
-	`
-
-	if utils.CleanString(result) != utils.CleanString(target) {
-		t.Fatal("Test failed")
-	}
-
-	text = "Limbe Leaf"
-
-	result = m.LoadMenu(session.CurrentMenu, session, phoneNumber, text, "")
-
-	target = `
-Gross Pay: (100000)
-
-00. Main Menu
-01. Keep
-98. Back
-99. Cancel
-	`
-
-	if utils.CleanString(result) != utils.CleanString(target) {
-		t.Fatal("Test failed")
-	}
-
-	text = "01"
-
-	result = m.LoadMenu(session.CurrentMenu, session, phoneNumber, text, "")
-
-	target = `
-Net Pay: (90000)
-
-00. Main Menu
-01. Keep
-98. Back
-99. Cancel
-	`
-
-	if utils.CleanString(result) != utils.CleanString(target) {
-		t.Fatal("Test failed")
-	}
-
-	text = "01"
-
-	result = m.LoadMenu(session.CurrentMenu, session, phoneNumber, text, "")
-
-	target = `
-Job Title: (Driver)
-
-00. Main Menu
-01. Keep
-98. Back
-99. Cancel
-	`
-
-	if utils.CleanString(result) != utils.CleanString(target) {
-		t.Fatal("Test failed")
-	}
-
-	text = "01"
-
-	result = m.LoadMenu(session.CurrentMenu, session, phoneNumber, text, "")
-
-	target = `
-Employer Address: (Kanengo)
-
-00. Main Menu
-01. Keep
-98. Back
-99. Cancel
-	`
-
-	if utils.CleanString(result) != utils.CleanString(target) {
-		t.Fatal("Test failed")
-	}
-
-	text = "P.O. Box 1678, Kanengo"
-
-	result = m.LoadMenu(session.CurrentMenu, session, phoneNumber, text, "")
-
-	target = `
-Employer Phone: (01789987)
-
-00. Main Menu
-01. Keep
-98. Back
-99. Cancel
-	`
-
-	if utils.CleanString(result) != utils.CleanString(target) {
-		t.Fatal("Test failed")
-	}
-
-	text = "09884746363"
-
-	result = m.LoadMenu(session.CurrentMenu, session, phoneNumber, text, "")
-
-	target = `
-Period Employed In Months: (36)
-
-00. Main Menu
-01. Keep
-98. Back
-99. Cancel
-	`
-
-	if utils.CleanString(result) != utils.CleanString(target) {
-		t.Fatal("Test failed")
-	}
-
-	text = "01"
-
-	result = m.LoadMenu(session.CurrentMenu, session, phoneNumber, text, "")
-
-	target = `
-Highest Qualification: (Secondary)
-1. Tertiary
-2. Secondary
-3. Primary
-4. None
-
-00. Main Menu
-01. Keep
-98. Back
-99. Cancel
-	`
-
-	if utils.CleanString(result) != utils.CleanString(target) {
-		t.Fatal("Test failed")
-	}
-
-	text = "1"
-
-	result = m.LoadMenu(session.CurrentMenu, session, phoneNumber, text, "")
-
-	target = `
-Summary
-- Employer Name: Limbe Leaf
-- Gross Pay: 100000
-- Net Pay: 90000
-- Job Title: Driver
-- Employer Address: P.O. Box 1678, Kanengo
-- Employer Phone: 09884746363
-- Period Employed In Months: 36
-- Highest Qualification: Tertiary
-
-0. Submit
-00. Main Menu
-98. Back
-99. Cancel
-	`
-
-	if utils.CleanString(result) != utils.CleanString(target) {
-		t.Fatal("Test failed")
-	}
-
-	text = "0"
-
-	result = m.LoadMenu(session.CurrentMenu, session, phoneNumber, text, "")
-
-	target = `
-CON Choose Activity
-1. Member Details 
-2. Occupation Details (*)
-3. Contact Details 
-4. Next of Kin Details 
-5. Beneficiaries 
-6. View Member Details
-
-00. Main Menu
-	`
-
-	if utils.CleanString(result) != utils.CleanString(target) {
-		t.Fatal("Test failed")
-	}
-
-	refData = map[string]any{
-		"employerAddress":        "P.O. Box 1678, Kanengo",
-		"employerName":           "Limbe Leaf",
-		"employerPhone":          "09884746363",
-		"grossPay":               "100000",
-		"highestQualification":   "Tertiary",
-		"jobTitle":               "Driver",
-		"netPay":                 "90000",
-		"periodEmployedInMonths": "36",
-	}
+	fmt.Println(result)
 
 	if data == nil {
 		t.Fatal("Test failed")
